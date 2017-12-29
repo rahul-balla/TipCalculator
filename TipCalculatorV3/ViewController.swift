addc71642c24cadf843bbc94ed637b6eba86627b
Rahul Balla <balla.rahul3@gmail.com>
2017-12-29 11:17:22 +0700

    Initial Commit

---
+++ TipCalculatorV3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculatorV3
+//
+//  Created by Rahul Balla on 12/29/17.
+//  Copyright © 2017 Rahul Balla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
